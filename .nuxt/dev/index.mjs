87b70f4dc1897e36b6f01b263002747f3c53ed29
bronn2120 <vince@smarthometheaters.com>
2025-11-11 21:34:20 -0500

    Fixed persistent asset router warnings by targeting :src and static src patterns for logo.png

--- .nuxt/dev/index.mjs
+++ .nuxt/dev/index.mjs
@@ -2874,7 +2874,7 @@ const _nuKGrFIGZqkFRIwAbl8kR4XB1C68uHT5qpxsHc7N2s0 = defineNitroPlugin(async (ni
     const localeSegment = detector.route(event.path);
     const pathLocale = isSupportedLocale(localeSegment) && localeSegment || void 0;
     const path = (pathLocale && url.pathname.slice(pathLocale.length + 1)) ?? url.pathname;
-    if (!url.pathname.includes("/_i18n/RmLQkerI") && !isExistingNuxtRoute(path)) {
+    if (!url.pathname.includes("/_i18n/GvrqPndu") && !isExistingNuxtRoute(path)) {
       return;
     }
     const resolved = resolveRedirectPath(event, path, pathLocale, ctx.vueI18nOptions.defaultLocale, detector);
